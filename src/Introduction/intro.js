ed3d2cd2e7a245a634ba1dfb2e1f78ee9190da4d
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2022-08-11 12:45:42 +0300

    Update intro.js

--- src/Introduction/intro.js
+++ src/Introduction/intro.js
@@ -6,6 +6,7 @@ import {Mycontext} from '../contextfile/Mycon';
  
 class Intro extends Component{ 
     render() {
+        let age = new Date().getFullYear() - 1999;
         return (
   <Mycontext.Consumer>
         {({theme})=>  
@@ -16,7 +17,7 @@ class Intro extends Component{
                         <p className="intro-name"  style={{color: theme=="dark" ? "" : "white"}}>
                     Hello, my name is Samer</p></strong>
                     <strong><p className="intro-about" style={{color: theme=="dark" ? "black" : ""}}>
-                        I'm 22 years old, from Lebanon.  
+                        I'm {age} years old, from Lebanon.  
                     iam a thriving future sofware engineer, I've studied front end development such as React, 
                     l love to learn new kinds of technologies,In the beginning i started with the basics Html
                     and css, then i worked up my way to JAvascript the main logical tech to run websites,and 
